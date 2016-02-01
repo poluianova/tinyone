eb4179f2a5632adf7b893cc24c1b11fcca0e1915
poluianova <marinka0588@ya.ru>
2016-02-01 19:32:26 +0200

    menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-;(function(){
+;(function($){
 
-})();
+	$(function(){
+		var body = $("body"),
+			menuToggle = $(".ba-menu-toggle");
+
+		menuToggle.click(function(event){
+			event.preventDefault();
+			body.toggleClass("ba-menu-opened");
+		});
+	});
+
+})(jQuery);
